91c21d557623ab39ea264ab7daac8da4949ed772
Jessica Basilio <jessica.limabasilio@gmail.com>
2025-02-08 22:47:07 -0300

    inserção dos detalhes do produto. criou-se outra página para colocar o produto individualmente.

--- app/src/main/java/br/uemg/florescer/ProductDetails.kt
+++ app/src/main/java/br/uemg/florescer/ProductDetails.kt
@@ -1,20 +1,47 @@
 package br.uemg.florescer
 
+
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.uemg.florescer.databinding.ActivityProductDetailsBinding
 
 class ProductDetails : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductDetailsBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityProductDetailsBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_product_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        //window.statusBarColor = Color.parseColor("#E0E0E0")
+
+        val imgProduct = intent.extras!!.getInt("imgProduct")
+        val name = intent.extras!!.getString("name")
+        val price = intent.extras!!.getString("price")!!.toDouble()
+        val newPrice = price
+        val decimalFormat = java.text.DecimalFormat.getCurrencyInstance()
+
+        binding.imgProduct.setBackgroundResource(imgProduct)
+        binding.txtProductName.text = "$name"
+        binding.txtProductPrice.text = decimalFormat.format(price)
+
+        binding.btBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
+
+        binding.btToIncrease.setOnClickListener {
+
+
+
+            }
+        }
+
     }
 }
